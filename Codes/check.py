e84bfef58be31050dc917ed539cec097b8319dea
Loki <athular7@gmail.com>
2017-11-23 22:09:28 +0530

    Redundancies

--- Codes/check.py
+++
@@ -1,8 +0,0 @@
-from basic import *
-
-
-qm = QuineMcCluskey(['A','B','C','D'])
-ones = [2,6,8,9,10,11,14,15]
-dontcares = []
-primes = qm.simplify(ones, dontcares)[1]
-print(qm.get_function(primes))
